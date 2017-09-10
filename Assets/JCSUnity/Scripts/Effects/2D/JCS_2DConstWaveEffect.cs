50984695deae61f98f68e27037e31cc2f5e200dc
jenchieh <jcs090218@gmail.com>
2017-09-11 06:19:24 +0800

    add randomize const wave effect

--- Assets/JCSUnity/Scripts/Effects/2D/JCS_2DConstWaveEffect.cs
+++ Assets/JCSUnity/Scripts/Effects/2D/JCS_2DConstWaveEffect.cs
@@ -48,6 +48,18 @@ namespace JCSUnity
         [SerializeField]
         private JCS_Axis mAxis = JCS_Axis.AXIS_Y;
 
+        [Header("- Randomize Setting (JCS_2DConstWaveEffect)")]
+
+        [Tooltip("Randomize a bit the amplitude value at start.")]
+        [SerializeField]
+        [Range(0.0f, 10.0f)]
+        private float mRandomizeAmplitudeAtStart = 0.0f;
+
+        [Tooltip("Randomize a bit the frequency value at start.")]
+        [SerializeField]
+        [Range(0.0f, 10.0f)]
+        private float mRandomizeFrequencyAtStart = 0.0f;
+
         /*******************************************/
         /*           Protected Variables           */
         /*******************************************/
@@ -61,6 +73,12 @@ namespace JCSUnity
         /*******************************************/
         /*            Unity's function             */
         /*******************************************/
+        private void Awake()
+        {
+            RandomizeAmplitude();
+            RandomizeFrequency();
+        }
+
         private void Update()
         {
             if (!mEffect)
@@ -106,6 +124,28 @@ namespace JCSUnity
 
         //----------------------
         // Private Functions
+
+        /// <summary>
+        /// Randomize a bit of amplitude value.
+        /// </summary>
+        private void RandomizeAmplitude()
+        {
+            if (mRandomizeAmplitudeAtStart == 0.0f)
+                return;
+
+            mAmplitude += JCS_Random.Range(-mRandomizeAmplitudeAtStart, mRandomizeAmplitudeAtStart);
+        }
+
+        /// <summary>
+        /// Randomize a bit of frequency value.
+        /// </summary>
+        private void RandomizeFrequency()
+        {
+            if (mRandomizeFrequencyAtStart == 0.0f)
+                return;
+
+            mFrequency += JCS_Random.Range(-mRandomizeFrequencyAtStart, mRandomizeFrequencyAtStart);
+        }
 
     }
 }
