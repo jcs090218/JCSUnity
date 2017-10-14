98963d193c5c774e1da1e1fffa71e389a96a21ba
jenchieh <jcs090218@gmail.com>
2017-10-14 17:02:13 +0800

    add listen to keyboard input, now compatible with desktop : keyboard.

--- Assets/JCSUnity/Scripts/GUI/JCS_GamePadButton.cs
+++ Assets/JCSUnity/Scripts/GUI/JCS_GamePadButton.cs
@@ -14,7 +14,8 @@ using UnityEngine;
 namespace JCSUnity
 {
     /// <summary>
-    /// Button will listen to the gamepad.
+    /// Button will listen to the gamepad. Note it compatible with
+    /// PC/desktop too.
     /// </summary>
     public class JCS_GamePadButton
         : JCS_Button
@@ -30,14 +31,6 @@ namespace JCSUnity
 
         [Header("** Runtime Variables (JCS_GamePadButton) **")]
 
-        [Tooltip("Key to trigger this button")]
-        [SerializeField]
-        private JCS_JoystickButton mKeyToListen = JCS_JoystickButton.NONE;
-
-        [Tooltip("Which joystick should listen?")]
-        [SerializeField]
-        private JCS_JoystickIndex mJoystickLitener = JCS_JoystickIndex.FROM_ALL_JOYSTICK;
-
         [Tooltip("Ignore the top two variables, listen to any key on the keyboard/gamepad.")]
         [SerializeField]
         private bool mListenToAnyKey = false;
@@ -46,6 +39,24 @@ namespace JCSUnity
         [SerializeField]
         private JCS_KeyActionType mKeyActionType = JCS_KeyActionType.KEY_DOWN;
 
+
+        [Header("- Keyboard (JCS_GamePadButton)")]
+
+        [Tooltip("Key to trigger this button")]
+        [SerializeField]
+        private KeyCode mKKeyToListen = KeyCode.None;
+
+
+        [Header("- Game Pad (JCS_GamePadButton)")]
+
+        [Tooltip("Key to trigger this button")]
+        [SerializeField]
+        private JCS_JoystickButton mJKeyToListen = JCS_JoystickButton.NONE;
+
+        [Tooltip("Which joystick should listen?")]
+        [SerializeField]
+        private JCS_JoystickIndex mJoystickLitener = JCS_JoystickIndex.FROM_ALL_JOYSTICK;
+
         /*******************************************/
         /*           Protected Variables           */
         /*******************************************/
@@ -53,7 +64,8 @@ namespace JCSUnity
         /*******************************************/
         /*             setter / getter             */
         /*******************************************/
-        public JCS_JoystickButton KeyToListen { get { return this.mKeyToListen; } set { this.mKeyToListen = value; } }
+        public KeyCode KKeyToListen { get { return this.mKKeyToListen; } set { this.mKKeyToListen = value; } }
+        public JCS_JoystickButton JKeyToListen { get { return this.mJKeyToListen; } set { this.mJKeyToListen = value; } }
         public JCS_JoystickIndex JoystickLitener { get { return this.mJoystickLitener; } set { this.mJoystickLitener = value; } }
         public bool ListenToAnyKey { get { return this.mListenToAnyKey; } set { this.mListenToAnyKey = value; } }
         public JCS_KeyActionType KeyActionType { get { return this.mKeyActionType; } set { this.mKeyActionType = value; } }
@@ -71,8 +83,15 @@ namespace JCSUnity
             }
             else
             {
-                if (JCS_Input.GetJoystickKeyByAction(mKeyActionType, mJoystickLitener, mKeyToListen))
+                
+                if (// listen to keyboard.
+                    JCS_Input.GetKeyByAction(mKeyActionType, mKKeyToListen) ||
+                    // listen to game pad.
+                    JCS_Input.GetJoystickKeyByAction(mKeyActionType, mJoystickLitener, mJKeyToListen)
+                    )
+                {
                     JCS_ButtonClick();
+                }
             }
         }
 
