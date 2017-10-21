3c59a54f88538282d6361d929ab7c97239d5af15
jenchieh <jcs090218@gmail.com>
2017-10-21 13:21:07 +0800

    Empty game pad button added for design usage.

---
+++ Assets/JCSUnity/Scripts/GUI/JCS_Buttons/JCS_EmptyGamePadButton.cs
@@ -0,0 +1,24 @@
+/**
+ * $File: JCS_EmptyGamePadButton.cs $
+ * $Date: 2017-10-21 13:09:38 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Empty gamepad button.
+    /// </summary>
+    public class JCS_EmptyGamePadButton
+        : JCS_GamePadButton
+    {
+
+    }
+}
