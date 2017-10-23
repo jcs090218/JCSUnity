b324e5d5b1c5758612c97269280361289487ba65
jenchieh <jcs090218@gmail.com>
2017-10-23 16:10:02 +0800

    echo game pad button for level design usage.

---
+++ Assets/JCSUnity/Scripts/GUI/JCS_Buttons/JCS_EchoGamePadButton.cs
@@ -0,0 +1,39 @@
+/**
+ * $File: JCS_EchoGamePadButton.cs $
+ * $Date: 2017-10-23 15:23:06 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright (c) 2017 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Echo out with game pad button
+    /// </summary>
+    public class JCS_EchoGamePadButton
+        : JCS_GamePadButton
+    {
+        [Header("** Runtime Variables (JCS_EchoGamePadButton) **")]
+
+        [Tooltip("String to echo out on the console window.")]
+        public string echoString = "echo Hello World!~";
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            SetCallback(OnClick);
+        }
+
+        private void OnClick()
+        {
+            JCS_Debug.Log(echoString);
+        }
+    }
+}
