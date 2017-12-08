d2b1062b1bb327dc30b8f1e027fe29734474c2c1
jenchieh <jcs090218@gmail.com>
2017-12-09 05:58:16 +0800

    add local position option.

--- Assets/JCSUnity/Scripts/Actions/3D/JCS_3DDistanceTileAction.cs
+++ Assets/JCSUnity/Scripts/Actions/3D/JCS_3DDistanceTileAction.cs
@@ -13,7 +13,6 @@ using UnityEngine;
 
 namespace JCSUnity
 {
-
     /// <summary>
     /// Move a game object in certain distance then set the game object
     /// back to original position.
@@ -39,8 +38,13 @@ namespace JCSUnity
         private bool mActive = true;
 
         [Tooltip("How long this game object could tracvel.")]
-        [SerializeField] [Range(0, 30000)]
+        [SerializeField]
+        [Range(0, 30000)]
         private float mDistance = 0;
+
+        [Tooltip("Use the local position instead of global position.")]
+        [SerializeField]
+        private bool mUseLocalPosition = false;
 
         //----------------------
         // Protected Variables
@@ -49,6 +53,7 @@ namespace JCSUnity
         //      setter / getter
         //------------------------------
         public bool Active { get { return this.mActive; } set { this.mActive = value; } }
+        public bool UseLocalPosition { get { return this.mUseLocalPosition; } set { this.mUseLocalPosition = value; } }
 
         //========================================
         //      Unity's function
@@ -58,7 +63,10 @@ namespace JCSUnity
             // NOTE(jenchieh): prevent if other component want to change
             // this position, we have the record position at function 
             // 'Start' rather than 'Awake'
-            this.mOriginPos = this.transform.localPosition;
+            if (mUseLocalPosition)
+                this.mOriginPos = this.transform.localPosition;
+            else
+                this.mOriginPos = this.transform.position;
         }
 
         private void Update()
@@ -68,14 +76,21 @@ namespace JCSUnity
 
             // Find out the distance between the current moved 
             // position and the starting position.
-            float distance = Vector3.Distance(this.transform.localPosition, mOriginPos);
+            float distance = 0.0f;
+            if (mUseLocalPosition)
+                distance = Vector3.Distance(this.transform.localPosition, mOriginPos);
+            else 
+                distance = Vector3.Distance(this.transform.position, mOriginPos);
 
             // check if the distance reach?
             if (distance < mDistance)
                 return;
 
             // set back to original position.
-            this.transform.localPosition = this.mOriginPos;
+            if (mUseLocalPosition)
+                this.transform.localPosition = this.mOriginPos;
+            else
+                this.transform.position = this.mOriginPos;
         }
 
         //========================================
