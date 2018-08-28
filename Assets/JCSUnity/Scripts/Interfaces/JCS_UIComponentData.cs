774f1384fbd630afd03d0c8ad009f629f35b1a67
jenchieh <jcs090218@gmail.com>
2018-08-28 23:48:02 +0800

    Add for all UI component data root interface.

---
+++ Assets/JCSUnity/Scripts/Interfaces/JCS_UIComponentData.cs
@@ -0,0 +1,23 @@
+/**
+ * $File: JCS_UIComponentData.cs $
+ * $Date: 2018-08-28 23:01:16 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © 2018 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// UI component data root struct.
+    /// </summary>
+    public interface JCS_UIComponentData
+    {
+        // empty..
+    }
+}
