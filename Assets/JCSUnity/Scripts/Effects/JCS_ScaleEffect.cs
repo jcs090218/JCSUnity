3385ca6259c5fb314fcbc5d19bbeaadb509930b6
jenchieh <jcs090218@gmail.com>
2018-09-06 19:15:52 +0800

    Make compatible with resizable screen with scale effect too, basically the same issue as slide effect.

--- Assets/JCSUnity/Scripts/Effects/JCS_ScaleEffect.cs
+++ Assets/JCSUnity/Scripts/Effects/JCS_ScaleEffect.cs
@@ -7,12 +7,12 @@
  *                   Copyright (c) 2016 by Shen, Jen-Chieh $
  */
 using UnityEngine;
+using UnityEngine.EventSystems;
 using System.Collections;
 
 
 namespace JCSUnity
 {
-
     /// <summary>
     /// Scale approach to a specific scale value.
     /// </summary>
@@ -25,6 +25,25 @@ namespace JCSUnity
 
         //----------------------
         // Private Variables
+
+        private Vector3 mTowardScale = Vector3.zero;
+        private Vector3 mRecordScale = Vector3.zero;
+        private Vector3 mTargetScale = Vector3.zero;
+
+
+        [Header("** Check Variables (JCS_ScaleEffect) **")]
+
+        [SerializeField]
+        private bool mEffect = false;
+
+        [Tooltip("")]
+        [SerializeField]
+        private JCS_PanelRoot mPanelRoot = null;
+
+        [Tooltip("")]
+        [SerializeField]
+        private EventTrigger mEventTrigger = null;
+
 
         [Header("** Initialize Variables (JCS_ScaleEffect) **")]
 
@@ -48,11 +67,21 @@ namespace JCSUnity
         [SerializeField]
         private Vector3 mScaleFriction = new Vector3(0.2f, 0.2f, 0.2f);
 
-        private Vector3 mTowardScale = Vector3.zero;
-        private Vector3 mRecordScale = Vector3.zero;
-        private Vector3 mTargetScale = Vector3.zero;
 
-        private bool mEffect = false;
+        [Header("- UI (JCS_ScaleEffect)")]
+
+        [Tooltip("Add event to event trigger system!")]
+        [SerializeField]
+        private bool mAutoAddEvent = true;
+
+        [Tooltip("Event trigger type to active the the slide effect.")]
+        [SerializeField]
+        private EventTriggerType mActiveEventTriggerType = EventTriggerType.PointerEnter;
+
+        [Tooltip("Event trigger type to deactive the the slide effect.")]
+        [SerializeField]
+        private EventTriggerType mDeactiveEventTriggerType = EventTriggerType.PointerExit;
+
 
         //----------------------
         // Protected Variables
@@ -69,11 +98,34 @@ namespace JCSUnity
         //------------------------------
         private void Start()
         {
+            // Only need it for the UI.
+            if (GetObjectType() == JCS_UnityObjectType.UI)
+            {
+                // Get panel root, in order to calculate the 
+                // correct distance base on the resolution.
+                mPanelRoot = this.GetComponentInParent<JCS_PanelRoot>();
+
+                if (mAutoAddEvent)
+                {
+                    // Event trigger is the must if we need to add the 
+                    // event to event trigger system.
+                    {
+                        this.mEventTrigger = this.GetComponent<EventTrigger>();
+                        if (this.mEventTrigger == null)
+                            this.mEventTrigger = this.gameObject.AddComponent<EventTrigger>();
+                    }
+
+                    JCS_Utility.AddEventTriggerEvent(mEventTrigger, mActiveEventTriggerType, JCS_OnMouseOver);
+                    JCS_Utility.AddEventTriggerEvent(mEventTrigger, mDeactiveEventTriggerType, JCS_OnMouseExit);
+                }
+            }
+
             Vector3 currentScale = this.transform.localScale;
 
             // record down the scale.
             mRecordScale = currentScale;
             mTargetScale = currentScale;
+
 
             SetTargetScale();
         }
@@ -81,7 +133,6 @@ namespace JCSUnity
         private void Update()
         {
             ScaleEffect();
-            JCS_OnMouseExit();
         }
 
         //========================================
@@ -89,26 +140,34 @@ namespace JCSUnity
         //------------------------------
         //----------------------
         // Public Functions
+
+        /// <summary>
+        /// Call it when is on mouse over.
+        /// 
+        /// Use in inspector for Event Trigger System. (Active)
+        /// </summary>
+        public void JCS_OnMouseOver(PointerEventData data)
+        {
+            JCS_OnMouseOver();
+        }
         public void JCS_OnMouseOver()
         {
-            mEffect = true;
-            mTargetScale = mTowardScale;
+            Active();
         }
 
+        /// <summary>
+        /// Call it When is on mouse exit.
+        /// 
+        /// Use in inspector for Event Trigger System. (Deactive)
+        /// </summary>
+        /// <returns></returns>
+        public void JCS_OnMouseExit(PointerEventData data)
+        {
+            JCS_OnMouseExit();
+        }
         public void JCS_OnMouseExit()
         {
-            if (!mEffect)
-                return;
-
-            if (GetObjectType() == JCS_UnityObjectType.UI)
-            {
-                UpdateUnityData();
-                if (JCS_Utility.MouseOverGUI(this.mRectTransform))
-                    return;
-            }
-
-            mEffect = false;
-            mTargetScale = mRecordScale;
+            Deactive();
         }
 
         /// <summary>
@@ -116,7 +175,9 @@ namespace JCSUnity
         /// </summary>
         public void Active()
         {
-            JCS_OnMouseOver();
+            mTargetScale = mTowardScale;
+
+            mEffect = true;
         }
 
         /// <summary>
@@ -124,7 +185,12 @@ namespace JCSUnity
         /// </summary>
         public void Deactive()
         {
-            JCS_OnMouseExit();
+            if (!mEffect)
+                return;
+
+            mTargetScale = mRecordScale;
+
+            mEffect = false;
         }
 
         //----------------------
@@ -132,6 +198,10 @@ namespace JCSUnity
 
         //----------------------
         // Private Functions
+
+        /// <summary>
+        /// Get the target scale.
+        /// </summary>
         private void SetTargetScale()
         {
             // so it will scale base on the starting scale.
@@ -139,11 +209,21 @@ namespace JCSUnity
 
             if (mScaleX)
             {
+                // mPanelRoot will be null is the object isn't
+                // UI game object.
+                if (mPanelRoot != null)
+                    mScaleValue.x /= mPanelRoot.PanelDeltaWidthRatio;
+
                 newTargetScale.x += mScaleValue.x;
             }
 
             if (mScaleY)
             {
+                // mPanelRoot will be null is the object isn't
+                // UI game object.
+                if (mPanelRoot != null)
+                    mScaleValue.y /= mPanelRoot.PanelDeltaWidthRatio;
+
                 newTargetScale.y += mScaleValue.y;
             }
 
@@ -156,6 +236,9 @@ namespace JCSUnity
             mTowardScale = newTargetScale;
         }
 
+        /// <summary>
+        /// Do the scale effect.
+        /// </summary>
         private void ScaleEffect()
         {
             Vector3 newScale = this.transform.localScale;
