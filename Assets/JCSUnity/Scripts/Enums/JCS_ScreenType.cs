40d0408f32e764e69f965a05ea1ddeec207c7bc3
jenchieh <jcs090218@gmail.com>
2018-09-08 15:54:42 +0800

    Add screen type for screen type handle choice.

---
+++ Assets/JCSUnity/Scripts/Enums/JCS_ScreenType.cs
@@ -0,0 +1,25 @@
+/**
+ * $File: JCS_ScreenType.cs $
+ * $Date: 2018-09-08 15:34:34 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © 2018 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Type of the screen handle.
+    /// </summary>
+    public enum JCS_ScreenType
+    {
+        NONE,
+        FORCE_ASPECT,
+        RESIZABLE,
+    }
+}
