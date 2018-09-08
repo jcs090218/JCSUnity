3ab601dad350af823b9d0963167ddffe15e98d27
jenchieh <jcs090218@gmail.com>
2018-09-08 15:45:44 +0800

    JCS_ScreenSettings implemented for screen handle.

---
+++ Assets/JCSUnity/Scripts/Settings/JCS_ScreenSettings.cs
@@ -0,0 +1,81 @@
+/**
+ * $File: JCS_ScreenSettings.cs $
+ * $Date: 2018-09-08 15:15:28 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *	                 Copyright © 2018 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Screen related settings.
+    /// </summary>
+    public class JCS_ScreenSettings
+        : JCS_Settings<JCS_ScreenSettings>
+    {
+
+        /*******************************************/
+        /*            Public Variables             */
+        /*******************************************/
+
+        /*******************************************/
+        /*           Private Variables             */
+        /*******************************************/
+
+        [Header("** Runtime Variables (JCS_ScreenManager) **")]
+
+        [Tooltip("Type of the screen handle.")]
+        public JCS_ScreenType SCREEN_TYPE = JCS_ScreenType.RESIZABLE;
+
+        /*******************************************/
+        /*           Protected Variables           */
+        /*******************************************/
+
+        /*******************************************/
+        /*             setter / getter             */
+        /*******************************************/
+
+        /*******************************************/
+        /*            Unity's function             */
+        /*******************************************/
+        private void Awake()
+        {
+            instance = CheckSingleton(instance, this);
+        }
+
+        /*******************************************/
+        /*              Self-Define                */
+        /*******************************************/
+        //----------------------
+        // Public Functions
+
+        //----------------------
+        // Protected Functions
+
+        /// <summary>
+        /// Instead of Unity Engine's scripting layer's DontDestroyOnLoad.
+        /// I would like to use own define to transfer the old instance
+        /// to the newer instance.
+        /// 
+        /// Every time when unity load the scene. The script have been
+        /// reset, in order not to lose the original setting.
+        /// transfer the data from old instance to new instance.
+        /// </summary>
+        /// <param name="_old"> old instance </param>
+        /// <param name="_new"> new instance </param>
+        protected override void TransferData(JCS_ScreenSettings _old, JCS_ScreenSettings _new)
+        {
+            _new.SCREEN_TYPE = _old.SCREEN_TYPE;
+        }
+
+        //----------------------
+        // Private Functions
+
+    }
+}
