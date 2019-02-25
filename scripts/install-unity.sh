610e9df6fd661920b94d3d848becc7b1fe05dce0
jenchieh <jcs090218@gmail.com>
2019-02-25 22:42:47 +0800

    Add travis test with unity version 2018.

--- scripts/install-unity.sh
+++ scripts/install-unity.sh
@@ -2,40 +2,43 @@
 
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
-BASE_URL=https://beta.unity3d.com/download/46dda1414e51
-VERSION=2017.2.0f3
+UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
+BASE_URL="https://download.unity3d.com/download_unity/0a46ddfcfad4"
+VERSION=2018.2.12f1
 
-UNITY_OSX_PACKAGE="MacEditorInstaller/Unity-$VERSION.pkg"
-UNITY_WINDOWS_TARGET_PACKAGE="MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-$VERSION.pkg"
+UNITY_OSX_PACKAGE_URL="$BASE_URL/MacEditorInstaller/Unity-$VERSION.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="$BASE_URL/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-$VERSION.pkg"
 
-
+# Downloads a file if it does not exist
 download() {
-	
-	FILE=$1
-	URL="$BASE_URL/$FILE"
 
-	#download package if it does not already exist in cache
-	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` ] ; then
+	URL=$1
+	FILE=`basename "$URL"`
+	
+	# Downloads a package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
 		echo "$FILE does not exist. Downloading from $URL: "
-		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$FILE"` "$URL"
+		mkdir -p "$UNITY_DOWNLOAD_CACHE"
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
 	else
 		echo "$FILE Exists. Skipping download."
 	fi
 }
 
+# Downloads and installs a package from an internet URL
 install() {
-	PACKAGE=$1
-	download "$PACKAGE"
-	
-	echo "Installing "`basename "$PACKAGE"`
-	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE"` -target /
+	PACKAGE_URL=$1
+	download $1
+
+	echo "Installing `basename "$PACKAGE_URL"`"
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
 }
 
 
 
 echo "Contents of Unity Download Cache:"
-ls "$UNITY_DOWNLOAD_CACHE"
+ls $UNITY_DOWNLOAD_CACHE
 
 echo "Installing Unity..."
-install "$UNITY_OSX_PACKAGE"
-install "$UNITY_WINDOWS_TARGET_PACKAGE"
+install $UNITY_OSX_PACKAGE_URL
+install $UNITY_WINDOWS_TARGET_PACKAGE_URL
