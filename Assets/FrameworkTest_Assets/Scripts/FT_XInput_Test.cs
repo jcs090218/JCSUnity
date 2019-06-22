048ecc4261a7845989d6245a676788d1881d26bc
jenchieh <jcs090218@gmail.com>
2019-06-22 12:34:01 +0800

    NS script changes.

--- Assets/FrameworkTest_Assets/Scripts/FT_XInput_Test.cs
+++ Assets/FrameworkTest_Assets/Scripts/FT_XInput_Test.cs
@@ -3,7 +3,7 @@
  * $Date: 2016-10-15 19:29:00 $
  * $Revision: $
  * $Creator: Jen-Chieh Shen $
- * $Notice: See LICENSE.txt for modification and distribution information 
+ * $Notice: See LICENSE.txt for modification and distribution information
  *	                 Copyright (c) 2016 by Shen, Jen-Chieh $
  */
 using UnityEngine;
