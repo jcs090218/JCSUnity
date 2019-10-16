79b3cdad5bd5f29e6e0d9d9afc073d3d8f80fc7d
JenChieh <jcs090218@gmail.com>
2019-10-16 15:13:13 +0800

    Add json save load test.

---
+++ Assets/FrameworkTest_Assets/Scripts/SaveLoad/FT_JSONGameData.cs
@@ -0,0 +1,29 @@
+﻿/**
+ * $File: FT_JSONGameData.cs $
+ * $Date: 2019-10-16 14:57:50 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2019 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JCSUnity;
+
+/// <summary>
+/// Test of the JSON game data.
+/// </summary>
+[System.Serializable]
+public class FT_JSONGameData 
+    : JCS_JSONGameData
+{
+    /* Variables */
+    public string Name = "";        // name of the player
+    public int Cash = 0;            // cash in the game
+
+    /* Setter/Getter */
+
+    /* Functions */
+
+}
