e24ef2c90ac91ab267e7d35cc1ec1417b490684c
JenChieh <jcs090218@gmail.com>
2020-12-17 21:04:09 +0800

    Use screen width.

--- Assets/JCSUnity/Scripts/GUI/JCS_ResizeUI.cs
+++ Assets/JCSUnity/Scripts/GUI/JCS_ResizeUI.cs
@@ -216,8 +216,8 @@ namespace JCSUnity
         {
             JCS_ScreenSettings ss = JCS_ScreenSettings.instance;
 
-            mWScale = (float)JCS_Screen.width / (float)ss.STARTING_SCREEN_SIZE.width;
-            mHScale = (float)JCS_Screen.height / (float)ss.STARTING_SCREEN_SIZE.height;
+            mWScale = (float)Screen.width / (float)ss.STARTING_SCREEN_SIZE.width;
+            mHScale = (float)Screen.height / (float)ss.STARTING_SCREEN_SIZE.height;
 
             mTargetScale = (mWScale > mHScale) ? mHScale : mWScale;
 
