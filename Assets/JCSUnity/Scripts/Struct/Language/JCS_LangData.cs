9a2d99bbfc08dfc6c8269a378326f8363fbeabb1
JenChieh <jcs090218@gmail.com>
2021-02-16 01:50:56 +0800

    Add lang data structure

---
+++ Assets/JCSUnity/Scripts/Struct/Language/JCS_LangData.cs
@@ -0,0 +1,43 @@
+/**
+ * $File: JCS_LangData.cs $
+ * $Date: 2021-02-15 23:30:52 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2021 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JCSUnity
+{
+    /// <summary>
+    /// Structure for specific language data.
+    ///
+    /// This data structure contains the following information.
+    ///   - Language label for this data structure.
+    ///   - String data representation for this language.
+    /// </summary>
+    [System.Serializable]
+    public class JCS_LangData
+    {
+        /* Variables */
+
+        [Tooltip("Label of this language.")]
+        [SerializeField]
+        private SystemLanguage mLanguage = SystemLanguage.Unknown;
+
+        [Tooltip("String data for this specific language.")]
+        [SerializeField]
+        private string mData = "";
+
+        /* Setter & Getter */
+
+        public SystemLanguage Language { get { return this.mLanguage; } }
+        public string Data { get { return this.mData; } }
+
+        /* Functions */
+
+    }
+}
---
+++ Assets/JCSUnity/Scripts/Struct/Language/JCS_LangDataList.cs
@@ -0,0 +1,36 @@
+/**
+ * $File: JCS_LangDataList.cs $
+ * $Date: 2021-02-16 00:20:29 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2021 by Shen, Jen-Chieh $
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JCSUnity
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [System.Serializable]
+    public class JCS_LangDataList
+    {
+        /* Variables */
+
+        [Header("** Initialize (JCS_LangDataList) **")]
+
+        [Tooltip("")]
+        [SerializeField]
+        private List<JCS_LangData> mLangData = null;
+
+        /* Setter & Getter */
+
+        public List<JCS_LangData> LangData { get { return this.mLangData; } }
+
+        /* Functions */
+
+    }
+}
