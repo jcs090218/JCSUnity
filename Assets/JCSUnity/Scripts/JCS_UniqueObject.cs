7f2f9ffbb948f5380b9ed325dee254e850ee5bf6
JenChieh <jcs090218@gmail.com>
2021-09-20 21:33:47 +0800

    Clean up singleton module

--- Assets/JCSUnity/Scripts/JCS_UniqueObject.cs
+++ Assets/JCSUnity/Scripts/JCS_UniqueObject.cs
@@ -17,22 +17,12 @@ namespace JCSUnity
     {
         /* Variables */
 
-        [Header("** Initialize Variables (JCS_UniqueObject) **")]
-
-        [Tooltip("Type of the singleton object.")]
-        [SerializeField]
-        private JCS_SingletonObject mType = JCS_SingletonObject.NONE;
-
         /* Setter & Getter */
-
-        public JCS_SingletonObject SingletonObjectType { get { return this.mType; } }
 
         /* Functions */
 
         private void Awake()
         {
-            CheckByType();
-
             // only the root object can do this.
             if (this.transform.parent == null)
                 DontDestroyOnLoad(this.gameObject);
@@ -41,58 +31,5 @@ namespace JCSUnity
                 JCS_Debug.LogWarning("Only the root object can be use DontDestoryOnLoad");
             }
         }
-
-        /// <summary>
-        /// Check the signleton by type.
-        ///
-        /// NOTE(jenchieh): Non-use.
-        /// </summary>
-        private void CheckByType()
-        {
-            //switch (mType)
-            //{
-            //    case JCS_SingletonObject.JCS_MANAGERS:
-            //        {
-            //            JCS_Managers[] jc = GameObject.FindObjectsOfType<JCS_Managers>();
-
-            //            if (jc.Length != 1)
-            //            {
-            //                JCS_Debug.LogError("Too many " + mType.ToString() + " in the scene...(Delete)");
-            //                DestroyImmediate(this.gameObject);
-            //                return;
-            //            }
-            //        }
-            //        break;
-
-            //    case JCS_SingletonObject.JCS_CANVAS:
-            //        {
-            //            JCS_Canvas[] jc = GameObject.FindObjectsOfType<JCS_Canvas>();
-
-            //            if (jc.Length != 1)
-            //            {
-            //                JCS_Debug.LogError("Too many " + mType.ToString() + " in the scene...(Delete)");
-            //                DestroyImmediate(this.gameObject);
-            //                return;
-            //            }
-
-            //        }
-            //        break;
-
-            //    case JCS_SingletonObject.JCS_GAMESETTINGS:
-            //        {
-            //            JCS_GameSettings[] jc = GameObject.FindObjectsOfType<JCS_GameSettings>();
-
-            //            if (jc.Length != 1)
-            //            {
-            //                JCS_Debug.LogError("Too many " + mType.ToString() + " in the scene...(Delete)");
-            //                DestroyImmediate(this.gameObject);
-            //                return;
-            //            }
-
-            //        } break;
-            //}
-
-        }
-
     }
 }
