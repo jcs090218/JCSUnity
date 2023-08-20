968b3ee0c71a035221c4d3688939e9d0738bf8c1
JenChieh <jcs090218@gmail.com>
2023-08-19 22:35:23 -0700

    feat: Use MyBox inspector lock

--- Assets/JCSUnity/Editor/JCSUnity_Hotkeys.cs
+++ Assets/JCSUnity/Editor/JCSUnity_Hotkeys.cs
@@ -46,30 +46,6 @@ namespace JCSUnity
             }
         }
 
-        [MenuItem(MI_BaseName + "/Toggle Inspector Lock &w", false, MI_BasePriority + 50)]
-        private static void ToggleInspectorLock()
-        {
-            if (_mouseOverWindow == null)
-            {
-                if (!EditorPrefs.HasKey("LockableInspectorIndex"))
-                    EditorPrefs.SetInt("LockableInspectorIndex", 0);
-                int i = EditorPrefs.GetInt("LockableInspectorIndex");
-
-                Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
-                Object[] findObjectsOfTypeAll = Resources.FindObjectsOfTypeAll(type);
-                _mouseOverWindow = (EditorWindow)findObjectsOfTypeAll[i];
-            }
-
-            if (_mouseOverWindow != null && _mouseOverWindow.GetType().Name == "InspectorWindow")
-            {
-                Type type = Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.InspectorWindow");
-                PropertyInfo propertyInfo = type.GetProperty("isLocked");
-                bool value = (bool)propertyInfo.GetValue(_mouseOverWindow, null);
-                propertyInfo.SetValue(_mouseOverWindow, !value, null);
-                _mouseOverWindow.Repaint();
-            }
-        }
-
         [MenuItem(MI_BaseName + "/Clear Console #&c", false, MI_BasePriority + 50)]
         private static void ClearConsole()
         {
