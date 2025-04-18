f4c914f1dedb1eaceaa6646aff600e8f58d97a57
JenChieh <jcs090218@gmail.com>
2025-04-18 16:22:33 -0700

    fix(UI): simplify usage of slider text display

--- Assets/JCSUnity/Scripts/UI/Slider/JCS_SliderTextDisplay.cs
+++ Assets/JCSUnity/Scripts/UI/Slider/JCS_SliderTextDisplay.cs
@@ -14,17 +14,6 @@ using MyBox;
 namespace JCSUnity
 {
     /// <summary>
-    /// A list of slider text display type.
-    /// </summary>
-    public enum SliderTextDisplay
-    {
-        CUSTOM,
-        VALUE,
-        MIN_VALUE,
-        MAX_VALUE,
-    }
-
-    /// <summary>
     /// Text that display slider's value.
     /// </summary>
     public class JCS_SliderTextDisplay : JCS_TextObject
@@ -37,25 +26,20 @@ namespace JCSUnity
         [SerializeField]
         private Slider mSlider = null;
 
-        [Tooltip("Text display type.")]
+        [Tooltip("Format text to display.")]
         [SerializeField]
-        private SliderTextDisplay mTextDisplay = SliderTextDisplay.VALUE;
+        private string mFormat = "{0} / {1}";
 
         [Tooltip("Place you want to round the decimal.")]
         [SerializeField]
         [Range(0, 15)]
         private int mRoundPlace = 2;
 
-        [Tooltip("Display with the custom text.")]
-        [SerializeField]
-        private string mCustomFormat = "{0} / {1}";
-
         /* Setter & Getter */
 
         public Slider slider { get { return this.mSlider; } set { this.mSlider = value; } }
-        public SliderTextDisplay TextDisplay { get { return this.mTextDisplay; } set { this.mTextDisplay = value; } }
+        public string Format { get { return this.mFormat; } set { this.mFormat = value; } }
         public int RoundPlace { get { return this.mRoundPlace; } set { this.mRoundPlace = value; } }
-        public string CustomFormat { get { return this.mCustomFormat; } set { this.mCustomFormat = value; } }
 
         /* Functions */
 
@@ -82,45 +66,10 @@ namespace JCSUnity
 
         private void OnValueChanged()
         {
-            if (mTextDisplay == SliderTextDisplay.CUSTOM)
-            {
-                this.text = string.Format(mCustomFormat,
-                    GetRoundValue(SliderTextDisplay.VALUE),
-                    GetRoundValue(SliderTextDisplay.MAX_VALUE),
-                    GetRoundValue(SliderTextDisplay.MIN_VALUE));
-
-                return;
-            }
-
-            double val = GetRoundValue(mTextDisplay);
-
-            this.text = val.ToString();
-        }
-
-        /// <summary>
-        /// Like the function `GetValue` but round the result.
-        /// </summary>
-        private double GetRoundValue(SliderTextDisplay displayType)
-        {
-            return Math.Round(GetValue(displayType), mRoundPlace);
-        }
-
-        /// <summary>
-        /// Return the slider text display type.
-        /// </summary>
-        private double GetValue(SliderTextDisplay displayType)
-        {
-            switch (displayType)
-            {
-                case SliderTextDisplay.VALUE:
-                    return mSlider.value;
-                case SliderTextDisplay.MIN_VALUE:
-                    return mSlider.minValue;
-                case SliderTextDisplay.MAX_VALUE:
-                    return mSlider.maxValue;
-            }
-
-            return 0.0f;
+            this.text = string.Format(mFormat,
+                    Math.Round(mSlider.value, mRoundPlace),
+                    Math.Round(mSlider.maxValue, mRoundPlace),
+                    Math.Round(mSlider.minValue, mRoundPlace));
         }
     }
 }
--- docs/ScriptReference/UI/Slider/JCS_SliderTextDisplay.md
+++ docs/ScriptReference/UI/Slider/JCS_SliderTextDisplay.md
@@ -4,9 +4,8 @@ Text that display slider's value.
 
 ## Variables
 
-| Name          | Description                                        |
-|:--------------|:---------------------------------------------------|
-| mSlider       | To update the text along with this slider's value. |
-| mTextDisplay  | Text display type.                                 |
-| mRoundPlace   | Place you want to round the decimal.               |
-| mCustomFormat | Display with the custom text.                      |
+| Name        | Description                                        |
+|:------------|:---------------------------------------------------|
+| mSlider     | To update the text along with this slider's value. |
+| mFormat     | Format text to display.                            |
+| mRoundPlace | Place you want to round the decimal.               |
