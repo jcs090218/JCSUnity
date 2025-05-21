bf2503752020cdada79b3db6bf97eb3b355561ef
JenChieh <jcs090218@gmail.com>
2025-05-21 01:40:38 -0700

    feat(UI): Expose more canvas functions

--- Assets/JCSUnity/Scripts/UI/JCS_CanvasComp.cs
+++ Assets/JCSUnity/Scripts/UI/JCS_CanvasComp.cs
@@ -1,3 +1,12 @@
+/**
+ * $File: JCS_CanvasComp.cs $
+ * $Date: $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information 
+ *                   Copyright (c) 2024 by Shen, Jen-Chieh $
+ */
+using System;
 using UnityEngine;
 
 namespace JCSUnity
@@ -18,6 +27,27 @@ namespace JCSUnity
 
         public JCS_Canvas canvas { get { return this.mCanvas; } }
 
+        public Action<JCS_Canvas> onShow
+        {
+            get => mCanvas.onShow;
+            set => mCanvas.onShow = value;
+        }
+        public Action<JCS_Canvas> onHide
+        {
+            get => mCanvas.onHide;
+            set => mCanvas.onHide = value;
+        }
+        public Action<JCS_Canvas> onShowFade
+        {
+            get => mCanvas.onShowFade;
+            set => mCanvas.onShowFade = value;
+        }
+        public Action<JCS_Canvas> onHideFade
+        {
+            get => mCanvas.onHideFade;
+            set => mCanvas.onHideFade = value;
+        }
+
         /* Functions */
 
         protected virtual void Awake()
@@ -25,8 +55,12 @@ namespace JCSUnity
             this.mCanvas = this.GetComponent<JCS_Canvas>();
         }
 
-        public virtual void Show() => mCanvas.Show();
-        public virtual void Hide() => mCanvas.Hide();
-        public virtual void ToggleVisibility() => mCanvas.ToggleVisibility();
+        public virtual bool IsShown() => mCanvas.IsShown();
+        public virtual void Show(bool mute = false) => mCanvas.Show(mute);
+        public virtual void Show(bool fade, bool mute = false) => mCanvas.Show(fade, mute);
+        public virtual void Hide(bool mute = false) => mCanvas.Hide(mute);
+        public virtual void Hide(bool fade, bool mute = false) => mCanvas.Hide(fade, mute);
+        public virtual void ToggleVisibility(bool mute = false) => mCanvas.ToggleVisibility(mute);
+        public virtual void ToggleVisibility(bool fade, bool mute = false) => mCanvas.ToggleVisibility(fade, mute);
     }
 }
--- docs/ScriptReference/UI/JCS_CanvasComp.md
+++ docs/ScriptReference/UI/JCS_CanvasComp.md
@@ -7,11 +7,3 @@ Component that requires the component `JCS_Canvas` without actually inherit the 
 | Name    | Description                           |
 |:--------|:--------------------------------------|
 | mCanvas | The canvas this component to control. |
-
-## Functions
-
-| Name             | Description                                       |
-|:-----------------|:--------------------------------------------------|
-| Show             | Show the canvas, so it's visible on the screen.   |
-| Hide             | Hide the canvas, so it's invisible on the screen. |
-| ToggleVisibility | Toggle the canvas' visibility.                    |
