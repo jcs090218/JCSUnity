38980a3d06501d173e990f1dbce9a1660dea9c02
JenChieh <jcs090218@gmail.com>
2025-12-16 04:17:47 +0800

    feat: Handle value change on min/max value on text slider

--- Assets/JCSUnity/Scripts/UI/Slider/JCS_SliderTextDisplay.cs
+++ Assets/JCSUnity/Scripts/UI/Slider/JCS_SliderTextDisplay.cs
@@ -20,6 +20,14 @@ namespace JCSUnity
     {
         /* Variables */
 
+        public Action onRefresh = null;
+
+        // Record for last min value.
+        private float mMinVal = 0;
+
+        // Record for last max value.
+        private float mMaxVal = 0;
+
         [Separator("Runtime Variables (JCS_TextSliderDisplay)")]
 
         [Tooltip("To update the text along with this slider's value.")]
@@ -51,6 +59,14 @@ namespace JCSUnity
         private void OnDisable()
         {
             RemoveListener();
+        }
+
+        private void Update()
+        {
+            if (mSlider == null)
+                return;
+
+            HandleMinMaxValueChanged();
         }
 
 #if UNITY_EDITOR
@@ -90,6 +106,21 @@ namespace JCSUnity
                     Math.Round(mSlider.value, mRoundPlace),
                     Math.Round(mSlider.maxValue, mRoundPlace),
                     Math.Round(mSlider.minValue, mRoundPlace));
+
+            onRefresh?.Invoke();
+        }
+
+        private void HandleMinMaxValueChanged()
+        {
+            if (mMinVal != mSlider.minValue ||
+                mMaxVal != mSlider.maxValue)
+            {
+                mMinVal = mSlider.minValue;
+                mMaxVal = mSlider.maxValue;
+
+                // Handle min/max value change.
+                Refresh();
+            }
         }
     }
 }
