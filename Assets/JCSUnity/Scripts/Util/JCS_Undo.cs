ecacd8b93303ce673109464b1b8c7efbe989b6bf
JenChieh <jcs090218@gmail.com>
2026-03-03 03:06:25 +0800

    feat: register complete shorthand

--- Assets/JCSUnity/Scripts/Util/JCS_Undo.cs
+++ Assets/JCSUnity/Scripts/Util/JCS_Undo.cs
@@ -8,6 +8,7 @@
  *                   Copyright (c) 2026 by Shen, Jen-Chieh $
  */
 using System;
+using System.Runtime.CompilerServices;
 using UnityEditor;
 
 namespace JCSUnity
@@ -27,13 +28,44 @@ namespace JCSUnity
         /// Like `Undo.RegisterCompleteObjectUndo` but a wrapper.
         /// </summary>
         public static void RegisterComplete(
-            UnityEngine.Object objectToUndo, string name, Action action)
+            UnityEngine.Object obj, string name, Action action)
         {
-            Undo.RegisterCompleteObjectUndo(objectToUndo, name);
+            var objs = new UnityEngine.Object[1] { obj };
+
+            RegisterComplete(objs, name, action);
+        }
+        public static void RegisterComplete(
+            UnityEngine.Object[] objs, string name, Action action)
+        {
+            Undo.RegisterCompleteObjectUndo(objs, name);
 
             action?.Invoke();
 
-            EditorUtility.SetDirty(objectToUndo);
+            foreach (UnityEngine.Object obj in objs)
+                EditorUtility.SetDirty(obj);
+        }
+
+        /// <summary>
+        /// Like `RegisterComplete` but automatically assign the 
+        /// undo operation name.
+        /// </summary>
+        public static void RegisterComplete(
+            UnityEngine.Object obj,
+            Action action,
+            [CallerMemberName] string name = "")
+        {
+            var objs = new UnityEngine.Object[1] { obj };
+
+            RegisterComplete(objs, action, name);
+        }
+        public static void RegisterComplete(
+            UnityEngine.Object[] objs,
+            Action action,
+            [CallerMemberName] string name = "")
+        {
+            string id = objs.GetType().Name;
+
+            RegisterComplete(objs, $"{id} ({name})", action);
         }
     }
 }
